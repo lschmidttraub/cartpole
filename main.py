8fb52ac53c3ef80c638734ee92a16f5d4c6946fc
lschmidttraub <leo.schmidttraub@gmail.com>
2026-01-11 21:30:30 +0100

    built environment + basic setup

---
+++ main.py
@@ -0,0 +1,18 @@
+import mujoco
+import mediapy as media
+import mujoco.viewer
+import os
+
+os.environ["LIBGL_ALWAYS_SOFTWARE"]="1"
+
+with open('envs/cartpole/cartpole.xml', 'r') as f:
+    xml = f.read()
+
+model = mujoco.MjModel.from_xml_string(xml)
+data = mujoco.MjData(model)
+mujoco.mj_resetDataKeyframe(model, data, 0)  # Reset to "start" keyframe
+
+with mujoco.Renderer(model) as renderer:
+    renderer.update_scene(data)
+
+    mujoco.viewer.launch(model, data)
